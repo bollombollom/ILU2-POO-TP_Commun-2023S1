6c357740e19facbde2a70eae640c7a574309faa1
BCH <bollombollom@yahoo.fr>
2023-11-11 16:12:28 +0100

    Préparation branchement IHM

---
+++ src/dialog/DialogReservation.java
@@ -0,0 +1,20 @@
+package dialog;
+
+import control.ControlReserverTable;
+import control.ControlVisualiserCarnetClientele;
+
+public class DialogReservation {
+
+	public DialogReservation(ControlReserverTable controlReserverTable,
+			ControlVisualiserCarnetClientele controlVisualiserCarnetClientele) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void initDialog() {
+	}
+
+	public void handleUserConnected(int numClient) {
+
+	}
+
+}
