66085acf7aa1f606113a605c136f45e7f081ee2f
BCH <bollombollom@yahoo.fr>
2023-11-11 14:14:23 +0100

    Q3: formulaires

---
+++ src/model/FormulaireSpectacle.java
@@ -0,0 +1,12 @@
+package model;
+
+public class FormulaireSpectacle extends Formulaire {
+
+	int zone;
+
+	public FormulaireSpectacle(int jour, int mois, int zone) {
+		super(jour, mois);
+		this.zone = zone;
+	}
+
+}
