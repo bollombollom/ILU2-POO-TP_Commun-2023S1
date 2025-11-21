fb1de6e98ff9a39e4cb78bab1f57992981ca3053
bch <bollombollom@yahoo.fr>
2025-11-21 19:44:10 +0100

    Relecture

--- test/model/CalendrierAnnuelTest.java
+++ test/model/CalendrierAnnuelTest.java
@@ -16,20 +16,26 @@ class CalendrierAnnuelTest {
 	}
 
 	@Test
-	void testCalendrierAnnuel() {
+	void testCalendrierAnnuel() throws InterruptedException {
 		assertNotNull(calendrier, "calendrier null");
+		Thread.sleep(1000);
+
 	}
 
 	@Test
-	void testEstLibre() {
+	void testEstLibre() throws InterruptedException {
 		assertTrue(calendrier.estLibre(31, 12), "31/12 devrait être libre");
+		Thread.sleep(1000);
+
 	}
 
 	@Test
-	void testReserver() {
+	void testReserver() throws InterruptedException {
 		assertTrue(calendrier.reserver(20, 10), "reservation du 20/10 devrait être possible");
 		assertFalse(calendrier.estLibre(20, 10), "20/10 devrait être occuper");
 		assertFalse(calendrier.reserver(20, 10), "reservation du 20/10 ne devrait pas être possible");
+		Thread.sleep(1000);
+
 	}
 
 }
